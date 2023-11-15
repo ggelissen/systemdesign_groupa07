136c4f36a3e51e8f60445f08a1b5af35a27e93da
Jin-1111 <150918604+Jin-1111@users.noreply.github.com>
2023-11-15 17:31:20 +0100

    Update Torsional stiffness.py

--- Torsional stiffness.py
+++ Torsional stiffness.py
@@ -1,21 +1,20 @@
 #Merge this file under the Rimaz code
 #Find torsional stiffness distribution for single-cell wing box k(y) over the half wing span by St Venant’s torsion constant
 # Find Am
-def Enclosed_Area_1(t_1,t_2,t_3,L_1,L_2):
-   #thickness of spar, thickness of upper plate, thickness of lower plate, width, length of front spar, length of rear spar
-   Am = (h_length - t_1) * (L_1 + L_2 - 2 * t_2 - 2 * t_3) / 2
+def Enclosed_Area_1(t_1,t_11,t_2,t_3,L_1,L_2):
+   #thickness of front spar, thickness of the rear spar, thickness of upper plate, thickness of lower plate, width, length of front spar, length of rear spar
+   Am = (h_length - t_1/2 - t_11/2) * (L_1 + L_2 - 2 * t_2 - 2 * t_3) / 2
    return Am
 #Find integral
-def line_integral(t_1,t_2,t_3,L_1,L_2,option):
-   #thickness of spar, thickness of upper plate, thickness of lower plate, length of front spar, length of rear spar
+def line_integral(t_1,t_11,t_2,t_3,L_1,L_2,option):
    #front spar integral
    I_1 = (L_1 - (t_2 + t_3)/2)/t_1
    #rear spar integral
-   I_2 = (L_2 - (t_2 + t_3)/2)/t_1
+   I_2 = (L_2 - (t_2 + t_3)/2)/t_11
    #upper part integral
-   I_3 = (l_up - 2 * t_1 * l_up / (2 * h_length) )/t_2
+   I_3 = (l_up - t_1 * l_up / (2 * h_length) - t_11 * l_up / (2 * h_length))/t_2
    #lower part integral
-   I_4 = (l_low - 2 * t_1 * l_low / (2 * h_length) )/t_3
+   I_4 = (l_low - t_1 * l_low / (2 * h_length) - t_11 * l_low / (2 * h_length))/t_3
    #Add all together
    I = I_1 + I_2 + I_3 + I_4
        integrals = {
@@ -25,37 +24,40 @@ def line_integral(t_1,t_2,t_3,L_1,L_2,option):
     }
    return integrals.get(option, None)
 #Find torsional constant
-def Torsional_constant_1(t_1,t_2,t_3,L_1,L_2):
-   J = 4 * Enclosed_Area_1(t_1,t_2,t_3,L_1,L_2) ** 2 / line_integral(t_1,t_2,t_3,L_1,L_2,1)
+def Torsional_constant_1(t_1,t_11,t_2,t_3,L_1,L_2):
+   J = 4 * Enclosed_Area_1(t_1,t_11,t_2,t_3,L_1,L_2) ** 2 / line_integral(t_1,t_11,t_2,t_3,L_1,L_2,1)
    return J
 #Find torsional stiffness
-def Torsional_stiffness_single_cell_1(t_1,t_2,t_3,L_1,L_2,y,G):
+def Torsional_stiffness_single_cell_1(t_1,t_11,t_2,t_3,L_1,L_2,y,G):
    #y is the distance from wing root, G is shear modulus
-   k = G * Torsional_constant_1(t_1,t_2,t_3,L_1,L_2)/y
+   k = G * Torsional_constant_1(t_1,t_11,t_2,t_3,L_1,L_2)/y
    return k
 
 #-----------------------------------------------------------------------------------------------------------------------------
 #Find torsional stiffness distribution for double-cell wing box k(y) over the half wing span by St Venant’s torsion constant
 #Fing length od the middle spar
-def Length_of_Middle_Spar(L_1,L_3):
+def Length_of_Middle_Spar(L_1,L_3,t_2,t_3):
    #L_3 is the distance from the front spar to midline of middle spar
-   L = L_1 * L_3 / h_length
+   L = L_1 * L_3 / h_length - t_2 * l_up / (2 * h_length) - t_3 * l_low / (2 * h_length)
    return L
 # Find Am
 def Enclosed_Area_2(t_1,t_2,t_3,L_1,L_3):
    #L_3 is the distance from the front spar to midline of middle spar
-   Am = (L_3 - t_1 / 2) * (Length_of_Middle_Spar(L_1,L_3) - (t_2 + t_3)/2) / 2
+   Am = (L_3 - t_1 / 2) * (Length_of_Middle_Spar(L_1,L_3,t_2,t_3) / 2
    return Am
-def Enclosed_Area_3(t_1,t_2,t_3,L_1,L_2,L_3):
-   Am = Enclosed_Area_1(t_1,t_2,t_3,L_1,L_2) - Enclosed_Area_2(t_1,t_2,t_3,L_1,L_3)
+def Enclosed_Area_3(t_1,t_11,t_2,t_3,L_1,L_2,L_3):
+   Am = Enclosed_Area_1(t_1,t_11,t_2,t_3,L_1,L_2) - Enclosed_Area_2(t_1,t_2,t_3,L_1,L_3)
    return Am
-def Rate_of_Twist_1(t_1,t_2,t_3,t_4,L_1,L_2,L_3,G):
+def Rate_of_Twist_1(t_1,t_11,t_2,t_3,t_4,L_1,L_2,L_3,G):
    #t_4 is the thickness of middle spar
-   LI =  [line_integral(t_1,t_2,t_3,L_1,L_2,2)/t_1 + ((L_3 * l_low / h_length)-t_1 * l_low/(2 * h_length))/t_3 + (Length_of_Middle_Spar(L_1,L_3) - (t_2 + t_3)/2)/t_4 + ((L_3*l_up/h_length)-t_1 * l_up/(2 * h_length))/t_2 , -(Length_of_Middle_Spar(L_1,L_3) - (t_2 + t_3)/2)/t_4]
-   RT = LI / 2 * Enclosed_Area_2(t_1,t_2,t_3,L_1,L_3) * G
+   LI =  [line_integral(t_1,t_11,t_2,t_3,L_1,L_2,2)/t_1 + ((L_3 * l_low / h_length)-t_1 * l_low/(2 * h_length))/t_3 + (Length_of_Middle_Spar(L_1,L_3,t_2,t_3) /t_4 + ((L_3*l_up/h_length)-t_1 * l_up/(2 * h_length))/t_2 , -(Length_of_Middle_Spar(L_1,L_3,t_2,t_3)/t_4]
+   RT = LI / (2 * Enclosed_Area_2(t_1,t_2,t_3,L_1,L_3) * G)
    return RT
-def Rate_of_Twist_1(t_1,t_2,t_3,t_4,L_1,L_2,L_3,G):
+def Rate_of_Twist_2(t_1,t_11,t_2,t_3,t_4,L_1,L_2,L_3,G):
    #t_4 is the thickness of middle spar
-   LI =  [-(Length_of_Middle_Spar(L_1,L_3) - (t_2 + t_3)/2)/t_4 , line_integral(t_1,t_2,t_3,L_1,L_2,3)/t_1 + ((L_3 * l_low / h_length)-t_1 * l_low/(2 * h_length))/t_3 + (Length_of_Middle_Spar(L_1,L_3) - (t_2 + t_3)/2)/t_4 + ((L_3*l_up/h_length)-t_1 * l_up/(2 * h_length))/t_2]
-   RT = LI / 2 * Enclosed_Area_3(t_1,t_2,t_3,L_1,L_2,L_3) * G
+   LI =  [-(Length_of_Middle_Spar(L_1,L_3,t_2,t_3)/t_4 , line_integral(t_1,t_11,t_2,t_3,L_1,L_2,3)/t_11 + (l_up-(L_3 * l_up / h_length)-t_11 * l_up/(2 * h_length))/t_2 + (Length_of_Middle_Spar(L_1,L_3,t_2,t_3)/t_4 +(l_low-(L_3 * l_low / h_length)-t_11 * l_low/(2 * h_length))/t_3]
+   RT = LI / (2 * Enclosed_Area_3(t_1,t_11,t_2,t_3,L_1,L_2,L_3) * G)
+   return RT
+def Rate_of_Twist_2(t_1,t_11,t_2,t_3,t_4,L_1,L_2,L_3,G):
+   RT = [2*Enclosed_Area_2(t_1,t_2,t_3,L_1,L_3)/G , 2*Enclosed_Area_3(t_1,t_11,t_2,t_3,L_1,L_2,L_3)/G]
    return RT
