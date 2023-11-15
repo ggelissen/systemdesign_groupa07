e07de9933ec021587765722189f10ce092bfe052
Jin-1111 <150918604+Jin-1111@users.noreply.github.com>
2023-11-15 21:57:00 +0100

    Update Torsional stiffness.py

--- Torsional stiffness.py
+++ Torsional stiffness.py
@@ -28,7 +28,7 @@ def Torsional_constant_1(t_1,t_11,t_2,t_3,L_1,L_2):
    J = 4 * Enclosed_Area_1(t_1,t_11,t_2,t_3,L_1,L_2) ** 2 / line_integral(t_1,t_11,t_2,t_3,L_1,L_2,1)
    return J
 #Find torsional stiffness
-def Torsional_stiffness_single_cell_1(t_1,t_11,t_2,t_3,L_1,L_2,y,G):
+def Torsional_stiffness_single_cell(t_1,t_11,t_2,t_3,L_1,L_2,y,G):
    #y is the distance from wing root, G is shear modulus
    k = G * Torsional_constant_1(t_1,t_11,t_2,t_3,L_1,L_2)/y
    return k
@@ -43,7 +43,7 @@ def Length_of_Middle_Spar(L_1,L_3,t_2,t_3):
 # Find Am
 def Enclosed_Area_2(t_1,t_2,t_3,L_1,L_3):
    #L_3 is the distance from the front spar to midline of middle spar
-   Am = (L_3 - t_1 / 2) * (Length_of_Middle_Spar(L_1,L_3,t_2,t_3) / 2
+   Am = (L_3 - t_1 / 2) *Length_of_Middle_Spar(L_1,L_3,t_2,t_3)/2
    return Am
 def Enclosed_Area_3(t_1,t_11,t_2,t_3,L_1,L_2,L_3):
    Am = Enclosed_Area_1(t_1,t_11,t_2,t_3,L_1,L_2) - Enclosed_Area_2(t_1,t_2,t_3,L_1,L_3)
@@ -51,14 +51,23 @@ def Enclosed_Area_3(t_1,t_11,t_2,t_3,L_1,L_2,L_3):
 def Rate_of_Twist_1(t_1,t_11,t_2,t_3,t_4,L_1,L_2,L_3,G):
    #t_4 is the thickness of middle spar
    LI =  [line_integral(t_1,t_11,t_2,t_3,L_1,L_2,2)/t_1 + ((L_3 * l_low / h_length)-t_1 * l_low/(2 * h_length))/t_3 + Length_of_Middle_Spar(L_1,L_3,t_2,t_3) /t_4 + ((L_3*l_up/h_length)-t_1 * l_up/(2 * h_length))/t_2 , -Length_of_Middle_Spar(L_1,L_3,t_2,t_3)/t_4]
-   RT = LI / (2 * Enclosed_Area_2(t_1,t_2,t_3,L_1,L_3) * G)
+   RT = LI.append(-2 * Enclosed_Area_2(t_1,t_2,t_3,L_1,L_3) * G)
    return RT
 def Rate_of_Twist_2(t_1,t_11,t_2,t_3,t_4,L_1,L_2,L_3,G):
    #t_4 is the thickness of middle spar
    LI =  [-Length_of_Middle_Spar(L_1,L_3,t_2,t_3)/t_4 , line_integral(t_1,t_11,t_2,t_3,L_1,L_2,3)/t_11 + (l_up-(L_3 * l_up / h_length)-t_11 * l_up/(2 * h_length))/t_2 + Length_of_Middle_Spar(L_1,L_3,t_2,t_3)/t_4 +(l_low-(L_3 * l_low / h_length)-t_11 * l_low/(2 * h_length))/t_3]
-   RT = LI / (2 * Enclosed_Area_3(t_1,t_11,t_2,t_3,L_1,L_2,L_3) * G)
+   RT = LI.append(-2 * Enclosed_Area_3(t_1,t_11,t_2,t_3,L_1,L_2,L_3) * G)
    return RT
-def Rate_of_Twist_3(t_1,t_11,t_2,t_3,t_4,L_1,L_2,L_3,G):
-   RT = [2*Enclosed_Area_2(t_1,t_2,t_3,L_1,L_3)/G , 2*Enclosed_Area_3(t_1,t_11,t_2,t_3,L_1,L_2,L_3)/G]
-   #whole thing is divided by J
-   return RT
+def Rate_of_Twist_Value(t_1,t_11,t_2,t_3,t_4,L_1,L_2,L_3,G):
+   import numpy as np
+   Matrix = np.array([[2*Enclosed_Area_2(t_1,t_2,t_3,L_1,L_3),2*Enclosed_Area_3(t_1,t_11,t_2,t_3,L_1,L_2,L_3),0.],Rate_of_Twist_1(t_1,t_11,t_2,t_3,t_4,L_1,L_2,L_3,G),Rate_of_Twist_2(t_1,t_11,t_2,t_3,t_4,L_1,L_2,L_3,G)])
+   righthandside = np.array([1.,0.,0.])
+   solution = np.linalg.solve(matrix,righthandside)
+   return solution[3]
+def Torsional_constant_2(t_1,t_11,t_2,t_3,t_4,L_1,L_2,L_3,G):
+   J = 1/(Rate_of_Twist_Value(t_1,t_11,t_2,t_3,t_4,L_1,L_2,L_3,G)*G)
+   return J
+def Torsional_stiffness_double_cell(t_1,t_11,t_2,t_3,t_4,L_1,L_2,L_3,y,G):
+   #y is the distance from wing root, G is shear modulus
+   k = G * Torsional_constant_2(t_1,t_11,t_2,t_3,t_4,L_1,L_2,L_3,G)/y
+   return k
