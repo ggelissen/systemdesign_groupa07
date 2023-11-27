274f2c6c329653142b1ecd3a5995c7332babc11d
ggelissen <g.m.j.gelissen@student.tudelft.nl>
2023-11-27 15:28:42 +0100

    update

--- Main_CSV.py
+++ Main_CSV.py
@@ -103,20 +103,5 @@ def main():
     plt.subplots_adjust(wspace=0.5)
     plt.show()
 
-    print(I_xfinal(0))
-    E = 68.9 * 10 ** 9
-
-    def load_integrand(y):
-        return (momentdistribution(y) * -1) / (I_xfinal(y) * E)
-
-    def deflection(y):
-        def load(y):
-            return integrate.quad(load_integrand, 0, y)[0]
-
-        deflection_result = integrate.quad(load, 0, y)[0]
-        return deflection_result
-
-    print(deflection(33))
-
 if __name__ == "__main__":
     main()
