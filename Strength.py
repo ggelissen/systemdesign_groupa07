58c55a48a56aa2c71114c47621528b335310348d
Jin-1111 <150918604+Jin-1111@users.noreply.github.com>
2023-12-13 17:09:12 +0100

    Update Strength.py

--- Strength.py
+++ Strength.py
@@ -32,47 +32,48 @@ for i in range(len(yvalues)):
    yield_stress.append(276000000)
 stress_up = np.array(stress_up[:-1])
 stress_down = np.array(stress_down[:-1]) * -1
-yield_stress = np.array(yield_stress[:-1])
+yield_stress_compress = np.array(yield_stress[:-1]) * -1
+yield_stress_tension = np.array(yield_stress[:-1])
 stress_up = stress_up * load_factor * safety_factor
 stress_down = stress_down * load_factor * safety_factor
 
 if load_factor>=0:
    compressive_stress = stress_up
    tension_stress = stress_down
-   margin_of_safety_compressive = - yield_stress / stress_up
-   i_for_compress = np.argmax(margin_of_safety_compressive > 2)
-   margin_of_safety_tensional = yield_stress / stress_down
-   i_for_tension = np.argmax(margin_of_safety_tensional > 2)
+   margin_of_safety_compressive = yield_stress_compress / stress_up
+   #i_for_compress = np.argmax(margin_of_safety_compressive > 2)
+   margin_of_safety_tensional = yield_stress_tension / stress_down
+   #i_for_tension = np.argmax(margin_of_safety_tensional > 2)
 else:
    compressive_stress = stress_down
    tension_stress = stress_up
-   margin_of_safety_compressive = - yield_stress / stress_down
-   i_for_compress = np.argmax(margin_of_safety_compressive > 2)
-   margin_of_safety_tensional = yield_stress / stress_up
-   i_for_tension = np.argmax(margin_of_safety_tensional > 2)
+   margin_of_safety_compressive = yield_stress_compress / stress_down
+   #i_for_compress = np.argmax(margin_of_safety_compressive > 2)
+   margin_of_safety_tensional = yield_stress_tension / stress_up
+   #i_for_tension = np.argmax(margin_of_safety_tensional > 2)
    
 plt.plot(yvalues[:-1], compressive_stress)
-plt.plot(yvalues[:-1], yield_stress)
+plt.plot(yvalues[:-1], yield_stress_compress)
 plt.xlabel("span")
 plt.ylabel("compress stress")
 #plt.title("q")
 plt.show()
 
 plt.plot(yvalues[:-1], tension_stress)
-plt.plot(yvalues[:-1], yield_stress)
+plt.plot(yvalues[:-1], yield_stress_tension)
 plt.xlabel("span")
 plt.ylabel("tension stress")
 #plt.title("q")
 plt.show()
 
-plt.plot(yvalues[:i_for_compress], margin_of_safety_compressive[:i_for_compress])
+plt.plot(yvalues[:-1], margin_of_safety_compressive)
 plt.plot([0,33.5], [1,1],linestyle='--', label='Dashed Line')
 plt.xlabel("span")
 plt.ylabel("margin of safety compress")
 #plt.title("q")
 plt.show()
 
-plt.plot(yvalues[:i_for_tension], margin_of_safety_compressive[:i_for_tension])
+plt.plot(yvalues[:-1], margin_of_safety_compressive)
 plt.plot([0,33.5], [1,1],linestyle='--', label='Dashed Line')
 plt.xlabel("span")
 plt.ylabel("margin of safety tension")
