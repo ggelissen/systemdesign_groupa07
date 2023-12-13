bff7066613bcfe963fad2db6ecf8832c26ee2fce
Jin-1111 <150918604+Jin-1111@users.noreply.github.com>
2023-12-13 16:27:17 +0100

    Update Strength.py

--- Strength.py
+++ Strength.py
@@ -14,19 +14,50 @@ from ColumnBuckling import bendingstress_stringer
 
 yvalues = np.arange(0, 33.5, 0.01)
 
-stress=[]
+stress_up=[]
+stress_down=[]
 yield_stres=[]
 n_spar=
 n_str1=
+safety_factor=
+load_factor=
 
 for i in range(len(yvalues)):
    t_1=
    w_u1=
    w_d1=
    A1=
-   stress.append(bendingstress_stringer(calculate_moment_of_inertia(n_spar, t_1, w_u1, w_d1, A1, n_str1, yvalues[i])[2]))
-   yield_stress.append(-276000000)
-stress=np.array(stress)
-stress = stress * 1.5
-yield_stress=np.array(yield_stress)
+   stress_up.append(bendingstress_stringer(calculate_moment_of_inertia(n_spar, t_1, w_u1, w_d1, A1, n_str1, yvalues[i])[2]))
+   stress_down.append(bendingstress_stringer(calculate_moment_of_inertia(n_spar, t_1, w_u1, w_d1, A1, n_str1, yvalues[i])[1]))
+   yield_stress.append(276000000)
+stress_up = np.array(stress_up[:-1])
+stress_down = np.array(stress_down[:-1])
+yield_stress = np.array(yield_stress[:-1])
+stress_up = stress_up * load_factor * safety_factor
+stress_down = stress_down * load_factor * safety_factor
+
+if load_factor>=0:
+   compressive_stress = stress_up
+   tensional_stress = stress_down
+   margin_of_safety_compressive = - yield_stress / stress_up
+   margin_of_safety_tensional = yield_stress / stress_down
+else:
+   compressive_stress = stress_down
+   tensional_stress = stress_up
+   margin_of_safety_compressive = - yield_stress / stress_down
+   margin_of_safety_tensional = yield_stress / stress_up
+   
+plt.plot(yvalues, stress)
+plt.plot(yvalues, yield_stress)
+plt.xlabel("span")
+plt.ylabel("stress")
+#plt.title("q")
+plt.show()
+
+plt.plot(yvalues[:len(margin_of_safety)], margin_of_safety)
+plt.plot([0,33.5], [1,1],linestyle='--', label='Dashed Line')
+plt.xlabel("span")
+plt.ylabel("margin of safety")
+#plt.title("q")
+plt.show()
 
